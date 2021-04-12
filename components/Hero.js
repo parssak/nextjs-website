f1b789f4cddb54960b702356ccc85285394867f8
Parssa Kyanzadeh <parssak@gmail.com>
2021-04-12 13:31:27 -0400

    Fixed up hero

--- components/Hero.js
+++ components/Hero.js
@@ -12,13 +12,13 @@ const HeroContainer = styled(Container)`
 const Subtitle = styled.h2`
     color: #c0c0c0;
     font-weight:400;
-    font-size: 3rem;
+    font-size: 2.6rem;
     margin: 0;
     margin-bottom: 4rem;
     margin-top: -0.5rem;
     letter-spacing: -0.05rem;
      @media (max-width: 800px) {
-        font-size: ${2.3}rem;
+        font-size: ${2}rem;
     }
     @media (max-width: 565px) {
         font-size: ${1.3}rem;
@@ -36,12 +36,17 @@ const Ball = styled.div`
     right: -100px;
     z-index: -1;
     background: radial-gradient(120.64% 80.49% at 49.9% 23.98%, rgba(88, 135, 255, 0) 51.66%, #951EBF 100%), radial-gradient(76.84% 51.26% at 49.9% 18.45%, #6951FF 0%, #5B1EBF 100%);
-`   
+`
+const HeroTitle = styled(Title)`
+    background: linear-gradient(271.88deg, #9C9AFF 3.74%, #BDACDD 61.1%, #FFD19A 105.72%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+`
 const Hero = () => {
     return (
         <HeroContainer>
             <NavBar />
-            <Title larger>Hi. I'm Parssa</Title>
+            <HeroTitle larger>Hi. I'm Parssa</HeroTitle>
             <Subtitle>Fullstack Developer & Designer</Subtitle>
             <Description>Based in Toronto, Canada 📍</Description>
             <Description>I’m passionate about creating useful tools through software.</Description>
