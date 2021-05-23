b5b9dbf57653aabb90e000a613346e00b64bcb34
parssa-airborneapp <parssak@gmail.com>
2021-05-23 08:33:11 -0400

    Added socials

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,12 +4,13 @@ import { ContactButton, Container, Description, Input, TextArea, Title, Label, T
 import Footer from './Footer';
 import { v4 as uuidv4 } from 'uuid';
 import emailjs from 'emailjs-com';
+import Socials from './Socials';
 
 const ContactContainer = styled.div`
     margin-top: 3rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
     @media (max-width: 900px) {
         flex-direction: column;
         margin-bottom: 7rem;
@@ -30,17 +31,17 @@ const ContactSectionContainer = styled(Container)`
 
 const Contact = ({ setAlertBox, contactRef }) => {
     const [lock, setLock] = useState(false);
-    
+
     const handleSubmit = e => {
         e.preventDefault();
         setLock(true);
         setAlertBox({ id: uuidv4(), text: 'Sending message...', type: 'pending' });
-        
+
         const combineMessage = {};
-        for (let child of e.target.children) 
+        for (let child of e.target.children)
             if (child.name)
-                    combineMessage[child.name] = child.value
-        
+                combineMessage[child.name] = child.value
+
         emailjs.send('service_cgfcq9r', 'template_u8oi1y8', combineMessage, 'user_uqFWPOLBqlRmtvJG5zsPg')
             .then((result) => {
                 console.log(result?.text)
@@ -48,26 +49,29 @@ const Contact = ({ setAlertBox, contactRef }) => {
             }, (error) => {
                 console.log(error?.text)
                 setAlertBox({ id: uuidv4(), text: 'An error occurred. :(', type: 'error' });
-            }).then(() => setTimeout(() => { setLock(false);}, 3000));
+            }).then(() => setTimeout(() => { setLock(false); }, 3000));
     }
 
     const handleAddClipboard = () => {
         navigator.clipboard.writeText('parssak@gmail.com')
-        setAlertBox({ id: uuidv4(), text: '🎉 Email copied to clipboard', type: 'success'});
+        setAlertBox({ id: uuidv4(), text: '🎉 Email copied to clipboard', type: 'success' });
     }
     return (
         <ContactSectionContainer ref={contactRef}>
             <Title>
                 Get In Touch
-                <TitleBall random={Math.random() * 360} left scale={0.5}/>
+                <TitleBall random={Math.random() * 360} left scale={0.5} />
             </Title>
             <ContactContainer>
-                <Description>
-                    If you want to get in touch or just
-                    say hi, you can email me
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                    <Description>
+                        If you want to get in touch or just
+                        say hi, you can email me
                     at <LinkText onClick={() => handleAddClipboard()}> parssak@gmail.com</LinkText>,
                     or use this form, and it’ll go right to my inbox.
                 </Description>
+                    <Socials />
+                </div>
                 <ContactForm onSubmit={handleSubmit} disabled={lock}>
                     <Label htmlFor="name">Name</Label>
                     <Input type="text" name="user_name" />
