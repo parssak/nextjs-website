b636ed7c9416650d8d9ff83620781bb238d39f26
parssak <parssak@gmail.com>
2023-01-22 12:23:52 -0500

    feat: cleaned up

--- src/pages/experiments/time-of-day.tsx
+++ src/pages/experiments/time-of-day.tsx
@@ -34,9 +34,9 @@ const timesToColors = {
   midnight: "from-[#354465] to-[#15162D] text-white",
   night: "from-[#536A9D] to-[#252750] text-white",
   morning: "from-[#AE8178] to-[#79ADE3]",
-  noon: "from-[#79ADE3] to-[#7B93B9]",
-  afternoon: "from-[#79ADE3] to-sky-200",
-  evening: "from-blue-500 to-sky-200",
+  noon: "from-[#84C6EC] to-[#79ADE3]",
+  afternoon: "from-[#79ADE3] to-[#7B93B9]",
+  evening: "from-[#4F7AB0] to-[#79ADE3]",
   dusk: "from-[#89240D] via-[#131322] to-[#131322]"
 } as const;
 
@@ -64,11 +64,12 @@ export default () => {
       <div className="h-96 relative overflow-hidden rounded-3xl">
         {Object.entries(timesToColors).map(([t, colors]) => (
           <div
-            className={`absolute inset-0
+            className={`absolute inset-0 transition-all
             ${t === timeOfDay ? "opacity-100 duration-500 z-20" : "opacity-0 duration-500 z-10"}`}
           >
             <div
-              className={`font-extrabold blur-xl scale-150 absolute h-full inset-0 w-full rounded-3xl bg-gradient-to-t transition-all ${colors}`}
+              className={`font-extrabold duration-500 blur-xl scale-x-150 absolute h-full inset-0 w-full rounded-3xl bg-gradient-to-t transition-all ${colors} 
+              ${t === timeOfDay ? "scale-y-125" : "scale-y-105"}`}
             ></div>
             <div className="absolute inset-0 grid place-items-center">
               <div className="text-white font-medium italic">{t}</div>
