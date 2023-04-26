c5875ca56006398a403c50b4325e72284fe8dd54
parssak <parssak@gmail.com>
2023-04-25 20:20:31 -0400

    feat: cleaned up example

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -24,19 +24,19 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
-        head_cell: "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+        head_cell: "text-theme-muted rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "text-center text-sm p-0 relative first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cx(
           "bg-transparent rounded hover:bg-theme-muted grid place-items-center",
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
-        day_outside: "text-muted-foreground opacity-50",
-        day_disabled: "text-muted-foreground opacity-50",
-        day_range_middle: "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "bg-theme-active text-theme-active hover:bg-theme-active hover:text-theme-active focus:bg-theme-active focus:text-theme-active",
+        day_today: "bg-orange-100 is-dark:bg-orange-950/50 text-orange-500 hover:bg-orange-100 is-dark:hover:bg-orange-950/50",
+        day_outside: "text-theme-muted opacity-50",
+        day_disabled: "text-theme-muted opacity-50",
+        day_range_middle: "aria-selected:bg-red-500 aria-selected:text-green-500",
         day_hidden: "invisible",
         ...classNames
       }}
