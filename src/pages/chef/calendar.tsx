742946c2a6fdef394fcac6e4889b55dfe544f434
parssak <parssak@gmail.com>
2023-04-25 20:02:59 -0400

    feat: added calendar example

---
+++ src/pages/chef/calendar.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Button, Checkbox, Popover, Text } from "@parssa/universal-ui";
+import { Calendar } from "components/Calendar";
+
+export default () => {
+  const [date, setDate] = React.useState<Date>();
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-theme-muted pb-[50vh]">
+      {date && <Text className="mb-size-4y">Selected Date: {date.toLocaleDateString()}</Text>}
+      <Popover.Root>
+        <Popover.Trigger>
+          <Button>Pick a date</Button>
+        </Popover.Trigger>
+        <Popover.Content>
+          <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+        </Popover.Content>
+      </Popover.Root>
+    </div>
+  );
+};
