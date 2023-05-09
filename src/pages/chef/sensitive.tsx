6ade5285cfefcee3a1b20742b86a2cce994ca6e0
parssak <parssak@gmail.com>
2023-05-09 18:28:38 -0400

    feat: cleaned up example

--- src/pages/chef/sensitive.tsx
+++ src/pages/chef/sensitive.tsx
@@ -4,16 +4,20 @@ import { Button, Input, Text } from "@parssa/universal-ui";
 export default () => {
   const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-theme-muted/50 pb-[50vh] gap-size-x">
+    <div className="min-h-screen flex max-w-xs mx-auto flex-col items-center justify-center bg-theme-muted/50 pb-[50vh] gap-size-x">
       <Button
-        className="rounded-full pl-size-2x pr-size-2x font-mono font-semibold tracking-normal"
+        className="w-full"
         size="lg"
-        theme="error"
+        theme={showPassword ? "success" : "error"}
         onClick={() => setShowPassword((p) => !p)}
       >
         Make Page {showPassword ? "Not Sensitive" : "Sensitive"}
       </Button>
-      {showPassword && <Input type="password" placeholder="Password" />}
+      {showPassword && (
+        <form className="w-full">
+          <Input className="w-full" type="password" placeholder="Password" />
+        </form>
+      )}
     </div>
   );
 };
