7b0af2eee0452e958b9e36c90b02e8cbdebd45e5
parssak <parssak@gmail.com>
2023-05-09 21:23:45 -0400

    feat: cleaned up

--- src/pages/chef/sensitive.tsx
+++ src/pages/chef/sensitive.tsx
@@ -4,20 +4,22 @@ import { Button, Input, Text } from "@parssa/universal-ui";
 export default () => {
   const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className="min-h-screen flex max-w-xs mx-auto flex-col items-center justify-center bg-theme-muted/50 pb-[50vh] gap-size-x">
-      <Button
-        className="w-full"
-        size="lg"
-        theme={showPassword ? "success" : "error"}
-        onClick={() => setShowPassword((p) => !p)}
-      >
-        Make Page {showPassword ? "Not Sensitive" : "Sensitive"}
-      </Button>
-      {showPassword && (
-        <form className="w-full">
-          <Input className="w-full" type="password" placeholder="Password" />
-        </form>
-      )}
+    <div className="min-h-screen flex mx-auto flex-col items-center justify-center bg-theme-muted/50 pb-[50vh] gap-size-x">
+      <div className="max-w-xs flex flex-col w-full items-center">
+        <Button
+          className="w-full"
+          size="lg"
+          theme={showPassword ? "success" : "error"}
+          onClick={() => setShowPassword((p) => !p)}
+        >
+          Make Page {showPassword ? "Not Sensitive" : "Sensitive"}
+        </Button>
+        {showPassword && (
+          <form className="w-full">
+            <Input className="w-full hidden" type="password" placeholder="Password" />
+          </form>
+        )}
+      </div>
     </div>
   );
 };
