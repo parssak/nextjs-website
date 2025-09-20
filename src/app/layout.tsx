2ade3cb19e8954a01a3874d87a925856bb3e1cc7
parssak <parssak@gmail.com>
2025-09-20 10:38:35 -0400

    added og stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,32 +2,58 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import ogImage from "@/public/og-image.png";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
+const title = "parssa kyanzadeh";
+const description = "product engineer, whom enjoys engineering products™";
+const ogUrl = "https://parssak.com";
 export const metadata: Metadata = {
-  title: "parssa kyanzadeh",
-  description: "product engineer, whom enjoys engineering products™"
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description,
+    url: ogUrl,
+    siteName: title,
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+        alt: "parssa kyanzadeh, product engineer"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description,
+    images: [ogImage.src],
+    site: ogUrl
+  },
+  icons: {
+    icon: "/favicon.ico"
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
         <Providers>{children}</Providers>
       </body>
     </html>
